593524e324ab176f72d202a5ea84c52b748e4f7b
Alex J. Cao <alexjcao@hotmail.com>
2020-08-05 21:26:23 -0700

    finished task 2

--- index.js
+++ index.js
@@ -1,113 +1,125 @@
-// // ⭐️ Example Challenge START ⭐️
+// ⭐️ Example Challenge START ⭐️
 
-// /**
-//  * ### Challenge `processFirstItem`
-//  * 
-//  * @instructions
-//  * Implement a higher-order function called `processFirstItem`.
-//  * It takes two arguments:
-//  * @param stringList an array of strings.
-//  * @param callback function that takes a string as its argument.
-//  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
-//  * 
-//  * Example of usage of this higher-order function:
-//  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
-//  * should return 'foofoo'.
-// */
-// function processFirstItem(stringList, callback) {
-//   return callback(stringList[0])
+/**
+ * ### Challenge `processFirstItem`
+ * 
+ * @instructions
+ * Implement a higher-order function called `processFirstItem`.
+ * It takes two arguments:
+ * @param stringList an array of strings.
+ * @param callback function that takes a string as its argument.
+ * @returns the result of invoking `callback` with the FIRST element in `stringList`.
+ * 
+ * Example of usage of this higher-order function:
+ * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
+ * should return 'foofoo'.
+*/
+function processFirstItem(stringList, callback) {
+  return callback(stringList[0])
+}
+
+// ⭐️ Example Challenge END ⭐️
+
+
+///// M V P ///////
+
+/* Task 1: `counterMaker`
+ * Study the code for counter1 and counter2. Answer the questions below.
+ * 
+ * 1. What is the difference between counter1 and counter2?
+ * 
+ * 2. Which of the two uses a closure? How can you tell?
+ * 
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+ *
+*/
+
+// counter1 code
+// function counterMaker() {
+//   let count = 0;
+//   return function counter() {
+//    return count++;
+//   }
 // }
 
-// // ⭐️ Example Challenge END ⭐️
+// const counter1 = counterMaker();
 
+// // counter2 code
+// let count = 0;
 
-// ///// M V P ///////
-
-// /* Task 1: `counterMaker`
-//  * Study the code for counter1 and counter2. Answer the questions below.
-//  * 
-//  * 1. What is the difference between counter1 and counter2?
-//  * 
-//  * 2. Which of the two uses a closure? How can you tell?
-//  * 
-//  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
-//  *
-// */
-
-// // counter1 code
-// // function counterMaker() {
-// //   let count = 0;
-// //   return function counter() {
-// //    return count++;
-// //   }
-// // }
-
-// // const counter1 = counterMaker();
-
-// // // counter2 code
-// // let count = 0;
-
-// // function counter2() {
-// //   return count++;
-// // }
-
-
-// /* Task 2: inning() 
-
-// Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
-
-// function inning(){
-
-//     /*Code Here*/
-//     let score = Math.floor(Math.random(3));
-//     return score;
+// function counter2() {
+//   return count++;
 // }
 
-// console.log(inning());
 
-// /* Task 3: finalScore()
+/* Task 2: inning() 
 
-// Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
+Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-// For example, 
+function inning(){
 
-// finalScore(inning, 9) might return: 
-// {
-//   "Home": 11,
-//   "Away": 5,
-// }
+    /*Code Here*/
+    let score = Math.floor(Math.random()*3);
+    return score;
+}
 
-// */ 
+console.log(inning());
 
-// function finalScore(/*code Here*/){
+/* Task 3: finalScore()
 
-//   /*Code Here*/
+Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
 
-// }
+For example, 
 
-// /* Task 4: 
+finalScore(inning, 9) might return: 
+{
+  "Home": 11,
+  "Away": 5,
+}
 
-// Create a function called `scoreboard` that accepts the following parameters: 
+*/ 
 
-// (1) Callback function `inning` that you wrote above
-// (2) A number of innings
+function finalScore(callback, scoreNum){
 
-// and returns the score at each pont in the game, like so:
+    /*Code Here*/
+    let fs = {
+        Home: 0,
+        Away: 0
+    };
+  
+    for(i=0; i < scoreNum; i++) {
+        let homeS = callback();
+        let awayS = callback();
+  
+        fs.Home += homeS;
+        fs.Away += awayS;   
+    }
+      return fs;
+  }
+  console.log(finalScore(inning, 9));
+  console.log(finalScore(inning, 11));
 
-// 1st inning: 0 - 2
-// 2nd inning: 1 - 3
-// 3rd inning: 1 - 3
-// 4th inning: 2 - 4
-// 5th inning: 4 - 6
-// 6th inning: 4 - 6
-// 7th inning: 4 - 6
-// 8th inning: 5 - 8
-// 9th inning: 6 - 10
+/* Task 4: 
 
-// Final Score: 6 - 10 */
+Create a function called `scoreboard` that accepts the following parameters: 
 
-// function scoreboard(/* CODE HERE */) {
-//   /* CODE HERE */
-// }
+(1) Callback function `inning` that you wrote above
+(2) A number of innings
 
-console.log("it's working")
+and returns the score at each pont in the game, like so:
+
+1st inning: 0 - 2
+2nd inning: 1 - 3
+3rd inning: 1 - 3
+4th inning: 2 - 4
+5th inning: 4 - 6
+6th inning: 4 - 6
+7th inning: 4 - 6
+8th inning: 5 - 8
+9th inning: 6 - 10
+
+Final Score: 6 - 10 */
+
+function scoreboard(/* CODE HERE */) {
+  /* CODE HERE */
+}
